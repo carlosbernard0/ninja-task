88bc2632dfe370edd628df10cd6061c0582ac402
Guilherme Frank <contatoguilhermefrank@gmail.com>
2023-11-22 09:18:07 -0300

    add react files

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NinjataskWrapper from './components/NinjataskWrapper';
+
+function App() {
+  return (
+    <div className="App">
+     <NinjataskWrapper />
+    </div>
+  );
+}
+
+export default App;
