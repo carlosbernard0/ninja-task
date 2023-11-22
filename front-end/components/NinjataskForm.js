228edf970e8d38ce96a37fc98c065cde3cdc1de2
Guilherme Frank <contatoguilhermefrank@gmail.com>
2023-11-22 09:47:52 -0300

    refatorando

--- front-end/components/NinjataskForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const NinjataskForm = () => {
-  return (
-    <div>NinjataskForm</div>
-  )
-}
--- front-end/components/NinjataskWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const NinjataskWrapper = () => {
-  return (
-    <div>NinjataskWrapper</div>
-  )
-}
